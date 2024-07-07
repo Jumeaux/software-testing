c31bc5668f2a2e869caf9dc3f8d777c8de850af7
Alhassane Diallo <alhassane.diallo@ubagroup.com>
2024-07-07 10:54:35 +0000

    get payments for customer

--- src/main/java/com/amigoscode/testing/payment/PaymentRepository.java
+++ src/main/java/com/amigoscode/testing/payment/PaymentRepository.java
@@ -1,7 +1,16 @@
 package com.amigoscode.testing.payment;
 
+import java.util.UUID;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
     
+
+    @Query(value = "select * from payment where customer_id=:customer_id", nativeQuery = true)
+    public List<Payment> findPaymentByCustomer(@Param("customer_id") UUID customerId );
 }
